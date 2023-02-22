eae0839249dbffa6ca988dbfa4976b696b153dc9
Orthancia Bajola <mawensima.bajola@gmail.com>
2023-02-21 21:31:51 -0800

    hard things

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function for printing alphabets in lower case
+ *
+ * return: returns 0 (Nothing per say but sucessful)
+ */
+void print_alphabet(void)
+{
+	char i;
+	/* This is a for loop to print the alphabetsone at a time */
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
