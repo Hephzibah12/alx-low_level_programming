98c330f648b0ec35fba117890b53ab5ddd9ae3df
Orthancia Bajola <mawensima.bajola@gmail.com>
2023-02-24 09:44:01 -0800

    task four completed

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints number from 0 to 9 except 2 and 4
+ *
+ * Return: 0(Successful)
+ */
+
+void print_most_numbers(void)
+{
+        int q = 0;
+
+        for (; q <= 9; q++)
+        {
+                if (q == 2 || q == 4)
+                {
+                        continue;
+                }
+                else
+                {
+                        _putchar(q + '0');
+                }
+        }
+        _putchar('\n');
+}
