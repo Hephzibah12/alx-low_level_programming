5f6a292391b9fae7b60dac32e1d9987205c50516
Orthancia Bajola <mawensima.bajola@gmail.com>
2023-02-24 09:34:42 -0800

    completed task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,9 +5,9 @@
  * @a: first integer
  * @b: second integer
  * Return: Returns the result of the multiplication
- 
- int mul(int a, int b)
- {
-         return (a * b);
-}
+ */
 
+int mul(int a, int b)
+{
+        return (a * b);
+}
