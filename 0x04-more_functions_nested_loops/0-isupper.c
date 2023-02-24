fe5de8073c6a8461922673c462c57b8ce4b6a4d2
Orthancia Bajola <mawensima.bajola@gmail.com>
2023-02-24 00:22:19 -0800

    hard

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_isupper - check if the letter is upper
+ * @x: The number to be checked
+ * Return 1 for upper letter or 0 for anyelse
+ */
+
+int _isupper(int x)
+
+{
+
+if (c >= 'A' && c <= 'Z')
+ {
+
+ return (1);
+else
+
+{
+
+ return (0);
+
+}
+
+
+}
+
+
