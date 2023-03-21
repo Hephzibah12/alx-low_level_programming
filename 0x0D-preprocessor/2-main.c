f8ebb404582b5d86714399fb6369e39e9c711197
Orthancia Bajola <mawensima.bajola@gmail.com>
2023-03-21 12:42:03 -0700

    task 2 completed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -10,16 +10,4 @@ int main(void)
 	printf("%s\n", __FILE__);
 	return (0);
 }
-#include <stdio.h>
-
-/**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
 
